2b7994f34b8e789e116acade71b71903aff17f62
vjblind <vjblind@users.noreply.github.com>
2023-04-27 03:44:33 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add a cube to the scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Add a grid to the scene
+const gridHelper = new THREE.GridHelper(10, 10);
+scene.add(gridHelper);
+
+// Add orbital controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+// Position the camera and cube
+camera.position.z = 5;
+cube.position.x = 2;
+
+// Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
